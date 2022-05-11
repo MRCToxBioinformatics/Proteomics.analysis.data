f8298ac99ac7372d9b8139783f92cb46f8d1b351
TomSmithCGAT <tss38@mrc-tox.cam.ac.uk>
2022-05-11 15:33:39 +0100

    adds LFQ data and updates docs

---
+++ R/raw_OOPS_RNase_LFQ.R
@@ -0,0 +1,20 @@
+#' PD output for LFQ of RNase +/- OOPS experiment
+#'
+#' F17-20 = RNase(-) reps 1-4
+#'
+#' F21-24 = RNase(+) reps 1-4
+#'
+#' @name raw_OOPS_RNase_LFQ_data
+#'
+#' @section OOPS_RNase_LFQ/LFQ_OOPS_RNase_PeptideGroups.txt:
+#'
+#' This is the peptide-level output from PD
+#'
+#' @section OOPS_RNase_LFQ/LFQ_OOPS_RNase_PSMs.txt:
+#'
+#' This is the PSM-level output from PD
+#' @section OOPS_RNase_LFQ/LFQ_OOPS_InputFiles.txt:
+#'
+#' This is the InputFiles output from PD
+NULL
+
---
+++ R/raw_OOPS_SILAC_data.R
@@ -0,0 +1,16 @@
+#' PD output for OOPS CL vs NC using SILAC labelled U-2 OS cells
+#'
+#' For replicates 1 & 2: H=CL, L=NC
+#'
+#' For replicates 3 & 4: H=CL, L=NC
+#'
+#' @name raw_OOPS_SILAC_data
+#'
+#' @section OOPS_SILAC/OOPS_*_PSMs.txt:
+#'
+#' These are PSM-level outputs from PD
+#'
+#' @section OOPS_SILAC/OOPS_*_PeptideGroups.txt:
+#'
+#' These are Peptide-level outputs from PD
+NULL
