5c5d4117f4b07f0ab49e6eb7689379e9f04746e2
TomSmithCGAT <tss38@cam.ac.uk>
2022-05-12 16:55:23 +0100

    adds TMT dataset

---
+++ R/raw_benchmark_TMT.R
@@ -0,0 +1,16 @@
+#' PD output for TMT experiment published here:
+#' https://pubs.acs.org/doi/10.1021/acsmeasuresciau.1c00053
+#'
+#' - Tags 1-4 = 1x yeast spike in
+#'
+#' - Tags 5-7 = 2x
+#'
+#' - Tags 8-10 = 6x
+#'
+#' @name raw_benchmark_TMT
+#'
+#' @section benchmark_TMT/benchmark_TMT_PSMs.txt.gz:
+#'
+#' This is the PSM-level output from PD
+NULL
+
